faaebe8147a3ab01a14cf8d73fcd933874ddc50e
michal <michal.jasinski11121@gmail.com>
2024-04-10 14:56:00 +0200

    Fix import

--- src/features/checkInOut/index.ts
+++ src/features/checkInOut/index.ts
@@ -1,3 +1,4 @@
+export { CheckInBooking } from "@/features/checkInOut/CheckInBooking";
 export { CheckOutButton } from "@/features/checkInOut/CheckOutButton";
 export { TodayActivity } from "@/features/checkInOut/TodayActivity";
 export { TodayItem } from "@/features/checkInOut/TodayItem";
